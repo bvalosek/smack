528741799e597e665d14c7128e6f5e5ddab77dff
Brandon Valosek <bvalosek@gmail.com>
2014-01-13 18:25:01 -0600

    index fix

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 module.exports = {
 
-  Binding    : require('./lib/Binding.js'),
-  Lifecycle  : require('./lib/Lifecycle.js'),
-  Observable : require('./lib/Observable.js'),
-  Repository : require('./lib/Repository.js'),
-  Resolver   : require('./lib/Resolver.js'),
-  Route      : require('./lib/Route.js'),
-  Router     : require('./lib/Router.js'),
-  Signal     : require('./lib/Signal.js'),
-  View       : require('./lib/View.js'),
-
-  util: {
-    extends : require('./lib/util/extends.js'),
-    getArgs : require('./lib/util/extends.js'),
-    log     : require('./lib/util/log.js'),
-    mixin   : require('./lib/util/mixin.js'),
-    stringy : require('./lib/util/stringy.js')
-  }
-
+    Binding        : require('./lib/Binding.js'),
+    ChangeNotifier : require('./lib/ChangeNotifier.js'),
+    Eventable      : require('./lib/Eventable.js'),
+    Observable     : require('./lib/Observable.js')
 };
